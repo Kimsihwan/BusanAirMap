8781e8770c7f2bf9f8c8f07cc3c688b54577bbd9
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-10-31 14:45:00 +0900

    핀을 하나 꽂는것

--- BusanAirMap/ViewController.swift
+++ BusanAirMap/ViewController.swift
@@ -14,7 +14,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 지도의 중심점
+        let location = CLLocationCoordinate2D(latitude: 35.180100, longitude: 129.081017)
+        // 반경 설정 숫자가 작을수록 줌인 클수록 줌아웃
+        let span = MKCoordinateSpan(latitudeDelta: 0.27, longitudeDelta: 0.27)
+        // region 설정
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        
+        //지도에 표시하기
+        dustMapView.setRegion(region, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = CLLocationCoordinate2D(latitude: 35.153245, longitude: 129.0616769)
+        pin.title = "경남공고"
+        pin.subtitle = "전포동 측정소"
+        dustMapView.addAnnotation(pin)
     }
-
 }
 
